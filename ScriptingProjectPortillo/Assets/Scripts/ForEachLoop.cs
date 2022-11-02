93bb04c044ddc773d641be612f63c35b4d6042c9
belimeow <711592@student.dusd.net>
2022-11-02 10:35:54 -0700

    cleaning up script
    fixed errors on do while loop script

--- ScriptingProjectPortillo/Assets/Scripts/ForEachLoop.cs
+++ ScriptingProjectPortillo/Assets/Scripts/ForEachLoop.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoWhileLoop : MonoBehaviour
+public class ForEachLoop : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        bool shouldContinue = false;
-
-        do
-        {
-            print("Hello World");
-
-        } while(shouldContinue == true);
+        
     }
 
     // Update is called once per frame
