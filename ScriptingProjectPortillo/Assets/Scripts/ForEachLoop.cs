ea231112c813fcc98bae27861f7e87131411a1d0
belimeow <711592@student.dusd.net>
2022-11-08 10:49:48 -0800

    Scripted
    I scripted and fixed all spelling errors

--- ScriptingProjectPortillo/Assets/Scripts/ForEachLoop.cs
+++ ScriptingProjectPortillo/Assets/Scripts/ForEachLoop.cs
@@ -7,12 +7,21 @@ public class ForEachLoop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string[] strings = new string[3];
+
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+
+        foreach (string item in strings)
+        {
+            print(item);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
