e51f295b1702b8068a4da9a5187a981df838dc59
belimeow <711592@student.dusd.net>
2023-01-31 10:58:33 -0800

    scripted
    Today i learned how to comment

---
+++ ScriptingProjectPortillo/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    // In this case; result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    //Mathf.Lerp takes 3 float parameters one representing the value to interpole form, another representing the value to interpole to and a final float represents how far 
+    Vector 3 from = new Vector3 (1f, 2f, 3f)
+    Vector3 to = new Vector3 (5f, 6f, 7f)
+
+    // here result = (4, 5, 6)
+    Vector result = Vector3.Lerp (from, to, 0.75f);
+    //Vector3.Lerp is 75% of the way between 1 and 5 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+       //light intensity increases from zero 
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f Time.deltaTime);
+       //This changes the light intensity per second 
+    }
+*/
+}
